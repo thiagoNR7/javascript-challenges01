e1f323c2c420deb2889def8a7e3c0592ea102c2e
Thiago NR <thiagokroos55gmail.com>
2023-09-28 18:57:06 -0300

    adicionando includes

--- desafio-2/funcionalidade.js
+++ desafio-2/funcionalidade.js
@@ -1,5 +1,6 @@
 const readline = require("readline");
-
+const palavrasValidas = [];
+let contador = 0;
 function RegisterInputData() {
   const rl = readline.createInterface({
     input: process.stdin,
@@ -14,25 +15,33 @@ function RegisterInputData() {
   });
 }
 function ProcessInputData(palavra) {
-  if (palavra.includes(Number)){
+  if (palavra.includes(Number)) {
+    palavrasValidas.push(palavra);
+    contador++;
     return false;
   }
-  if (palavra.includes('@,#,!')) {
+  if (palavra.includes("@,#,!")) {
     return false;
   }
   if (palavra.length <= 4) {
     return false;
+  
   }
   return true;
 }
+
 
 async function main() {
   const palavraDigitada = await RegisterInputData();
 
   if (ProcessInputData(palavraDigitada)) {
-    console.log("A palavra digitada foi:", palavraDigitada);
+    console.log(
+      `A sua palavra  tem ${palavraDigitada.length} caracteres e foi  registrada:`, palavraDigitada
+    );
   } else {
-    console.log("A palavra digitada não é valida:", palavraDigitada);
+    console.log(
+      ` ${palavraDigitada} A palavra digitada não é valida. Verifique e tente novamente!:`, palavraDigitada
+    );
   }
 }
 main();
